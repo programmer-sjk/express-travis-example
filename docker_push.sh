b4fc301e3763f5ed64fca35af1a97cfee02f53e1
sjk5766 <wjdrnr6662@gmail.com>
2020-06-24 15:18:34 +0900

    docker hub에 private repo 생성 후 push

--- docker_push.sh
+++ docker_push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker build -t sjk5766/express:0.0.1 .
-docker tag sjk5766/express:latest sjk5766/express:0.0.1
+#docker tag sjk5766/express:latest sjk5766/express:0.0.1
 docker push sjk5766/express:0.0.1
