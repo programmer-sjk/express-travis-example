f2003a4206b4ee431a5e9fdcf60d6c895a2da346
sjk5766 <wjdrnr6662@gmail.com>
2020-06-24 22:48:50 +0900

    build시 sjk5766 제외하고

--- docker_push.sh
+++ docker_push.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker build -t sjk5766/express:0.0.1 .
+docker build -t express:0.0.1 .
+docker tag express:0.0.1 sjk5766/express:0.0.1
 docker push sjk5766/express:0.0.1
