8d650dad66def1ecdb1dfa2f516c89d422ad37fc
sjk5766 <wjdrnr6662@gmail.com>
2020-06-24 15:10:36 +0900

    docker logout후 login 되도록 수정

--- docker_push.sh
+++ docker_push.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+docker logout
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker build -t sjk5766/express:0.0.1 .
 docker tag sjk5766/express:0.0.1 sjk5766/express:latest
