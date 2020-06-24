8a5f08a3b30cfa2f4e9ea11c86a9d6f902a8c289
sjk5766 <wjdrnr6662@gmail.com>
2020-06-24 21:40:58 +0900

    logout 다시 추가

--- docker_push.sh
+++ docker_push.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+docker logout
 docker build -t sjk5766/express:0.0.1 .
-#docker tag sjk5766/express:latest sjk5766/express:0.0.1
+#docker tag express:0.0.1 sjk5766/express:0.0.1
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push sjk5766/express:0.0.1
