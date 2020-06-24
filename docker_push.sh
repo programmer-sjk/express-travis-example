d8be700ec34f87d46eda764c337cf3021af48680
sjk5766 <wjdrnr6662@gmail.com>
2020-06-24 22:20:13 +0900

    local 성공한 파일 다시 시도

--- docker_push.sh
+++ docker_push.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
-docker logout
+
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker build -t sjk5766/express:0.0.1 .
-docker tag sjk5766/express:0.0.1 sjk5766/express:latest
-
 docker push sjk5766/express:0.0.1
-docker push sjk5766/express:latest
-
-#docker login -u YOUR_USERNAME -p YOUR_PASSWORD
-
-#docker build -t outsideris/demo:0.0.1 .
-
-#docker tag outsideris/demo:0.0.1 outsideris/demo:latest
-
-#docker push outsideris/demo:0.0.1
-#docker push outsideris/demo:latest
