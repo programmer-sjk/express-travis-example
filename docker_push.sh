49fb6e4f7cf9507860880a85b9c51b9e63a904d9
sjk5766 <wjdrnr6662@gmail.com>
2020-06-25 12:44:51 +0900

    docker_username -> docker_user

--- docker_push.sh
+++ docker_push.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 docker logout
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-echo "$DOCKER_USERNAME"
+echo "$DOCKER_USER"
+echo "$DOCKER_USER"
 docker build -t express:0.0.1 .
 docker tag express:0.0.1 sjk5766/express:0.0.1
 docker push sjk5766/express:0.0.1
