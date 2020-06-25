81826e3279e2fd4dee15d782b3bb9da0b4ad1043
sjk5766 <wjdrnr6662@gmail.com>
2020-06-25 12:47:16 +0900

    docker_user, docker_pass로 변경

--- docker_push.sh
+++ docker_push.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-docker logout
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-echo "$DOCKER_USER"
-echo "$DOCKER_USER"
+#docker logout
+echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
 docker build -t express:0.0.1 .
-docker tag express:0.0.1 sjk5766/express:0.0.1
+#docker tag express:0.0.1 sjk5766/express:0.0.1
 docker push sjk5766/express:0.0.1
