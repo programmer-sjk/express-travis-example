1f30456b354700e5492c9444a827c9ff07a9fb0a
sjk5766 <wjdrnr6662@gmail.com>
2020-06-25 12:41:43 +0900

    travis 환경정보 확인용 echo 추가

--- docker_push.sh
+++ docker_push.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-
+docker logout
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+echo "$DOCKER_USERNAME"
 docker build -t express:0.0.1 .
 docker tag express:0.0.1 sjk5766/express:0.0.1
 docker push sjk5766/express:0.0.1
